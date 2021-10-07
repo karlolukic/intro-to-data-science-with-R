2a5457a5973832581c72f41706b4354e564ff974
karlolukic <karlo.lukic@pm.me>
2021-10-07 09:29:53 +0200

    Prepare empty files for writing R code

--- 02_scripts/demonstration_of_r_script.R
+++ 02_scripts/demonstration_of_r_script.R
@@ -1,9 +1,4 @@
-# Title: 
-# Demonstration of R Script 
-# Author: 
-# Karlo Lukic
-# Date: 
-# 2021-10-07
-# Description: 
-# This script demonstrates the common usage of R scripts
+# Title: Demonstration of R Script 
+# Author: Karlo Lukic
+# Date: 2021-10-08
 
